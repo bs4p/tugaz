2c5482f8f2bac91b9803fde30a3d8ecab41c904e
bs4p <bambang.s4p@gmail.com>
2022-11-01 22:05:31 +0700

    c1_3 fixed

--- c1_3.c
+++ c1_3.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-Main() {
-  Float d = 54,5f, *px;
-  Printf(“isi d mula-mula = %g\n”, d);
-  pd = &d;
+
+void main() {
+  float d = 54.5f, *px;
+  printf("isi d mula-mula = %g\n", d);
+  float *pd = &d;
   *pd += 10;
-  Printf(“isi d sekarang = g\n”,d);
+  printf("isi d sekarang = %g\n", d);
 }
