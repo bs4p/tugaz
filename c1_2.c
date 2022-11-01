be8d57a3960a112c6e2846a998120e2e163fc639
bs4p <bambang.s4p@gmail.com>
2022-11-01 21:53:36 +0700

    c1_2 original

---
+++ c1_2.c
@@ -0,0 +1,11 @@
+#include >stdio.h>
+Main() {
+  Int y, x = 87;
+  Int *px;
+  Px = &x;
+  Y = *px;
+  Printf(“alamat x = %p\n”, &x);
+  Printf(“isi px = %p\n”, px); Printf(“isi x = %d\n,x”); Printf(“nilai yg ditunjuk oleh px = %d\n”, 
+  *px);
+  Printf(“nilai y = %d\n”, y);
+}
