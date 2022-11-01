fe1b129516cc93131d6c5b1580c4e7be91d12182
bs4p <bambang.s4p@gmail.com>
2022-11-01 21:57:20 +0700

    c1_2 fixed

--- c1_2.c
+++ c1_2.c
@@ -1,11 +1,13 @@
-#include >stdio.h>
-Main() {
-  Int y, x = 87;
-  Int *px;
-  Px = &x;
-  Y = *px;
-  Printf(“alamat x = %p\n”, &x);
-  Printf(“isi px = %p\n”, px); Printf(“isi x = %d\n,x”); Printf(“nilai yg ditunjuk oleh px = %d\n”, 
-  *px);
-  Printf(“nilai y = %d\n”, y);
+#include <stdio.h>
+
+void main() {
+  int y, x = 87;
+  int *px;
+  px = &x;
+  y = *px;
+  printf("alamat x = %p\n", &x);
+  printf("isi px = %p\n", px); 
+  printf("isi x = %d\n", x); 
+  printf("nilai yg ditunjuk oleh px = %d\n", *px);
+  printf("nilai y = %d\n", y);
 }
